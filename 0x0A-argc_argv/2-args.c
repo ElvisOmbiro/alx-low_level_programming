56eba56c40020f4fe8fe3b00c6fb83dbeee6be5a
ElvisOmbiro <elvisombiro99@gmail.com>
2023-08-27 14:22:34 +0000

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,8 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 
 /**
- * main - a program that prints all arguments it receives.
+ * main - a program that prints all the arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *

399b9e60b240d36d7d7ee6184279929bd0655a83
ElvisOmbiro <elvisombiro99@gmail.com>
2023-08-29 11:08:12 +0000

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - a function that inserts a new node at a given position.
+ * insert_nodeint_at_index - function that inserts new node at given position
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node

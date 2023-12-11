a9fbfda8721ac0774c374fff6a98e4495db9598b
ElvisOmbiro <elvisombiro99@gmail.com>
2023-12-11 06:23:07 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: subtracted final result
+ */
+
+int sub (int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of final result
+ */
+
+int add (int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication of final result
+ */
+
+int mul (int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * mod - mod of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of final result
+ */
+
+int mod (int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: division of final result
+ */
+
+int div (int a, int b)
+{
+	return (a / b);
+}

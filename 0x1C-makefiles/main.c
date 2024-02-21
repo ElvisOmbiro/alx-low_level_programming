44da856aaefb0706dd9d366dd521004a05f70119
ElvisOmbiro <elvisombiro99@gmail.com>
2024-02-21 02:07:43 -0800

    some ew files

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_school(void);
+/*
+ * main - entry point of the argument
+ * Return: Always
+ */
+
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
+
